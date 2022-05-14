d8f5dcfd12e44b84b3f373de5b45d13ec66396ea
Victor Robert <rinmai2019@gmail.com>
2022-05-14 15:45:53 +0700

    update

--- bai5.js
+++ bai5.js
@@ -1,24 +1,46 @@
-function baiTap1() {
-  //  B1: Dung DOM
-  var result1 = $("result1");
-  result1.style.display = "block";
-  result1.style.background = "aquamarine";
-
-  // var s = 0;
-  // var i = 0;
-  // while (s < 10000) {
-  //   i++;
-  //   s += i;
-  //   result1.innerHTML =  `Số nguyên dương nhỏ nhât: ${i}`;
-  // }
-  //D
-  //B2 khai bao biến, dùng vòng lặp for
-  var s = 0;
-  for (var i = 0; s < 10000; i++) {
-    s += i;
-    result1.innerHTML = `Số nguyên dương nhỏ nhât: ${i}`; //B3 In ra ket quả
-  }
-}
 function $(x) {
   return document.getElementById(x);
+}
+// B1: Tạo hàm xử lý in số nguyên tố
+function baiTap5() {
+  var result5 = $("result5");
+  result5.style.display = "block";
+  result5.style.color = "white";
+  //B2 Tạo biến nhapN gán giá trị số nhập từ bàn phím
+  var nhapN = +$("nhapN").value;
+  var ketQua = "";
+  // B3: Tạo vòng lặp for xử lý in số nguyên tố từ 1 -> nhapN
+  for (i = 1; i <= nhapN; i++) {
+    // Trường hợp i là số nguyên tố thì in ra ketQua
+    if (soNguyenTo(i)) {
+      ketQua += i + " ";
+    }
+  }
+  // B5: In kết quả ra theo từng vong lặp
+  result5.innerHTML = ketQua;
+}
+
+// B3: Tạo hàm với tham số n xác định n có phải số nguyên tố hay không
+function soNguyenTo(n) {
+  // Tạo biến cờ hiệu trả về giá trị boolean
+  // 2 là số chẵn duy nhất trong số nguyên tố nên tách riêng 1 trường hợp
+  // Nếu n < 2 hoặc n chia 2 có dư = 0 thì n không phải số nguyên tố
+  var NT = true;
+  if (n === 2) {
+    NT = true;
+  } else if (n < 2) {
+    NT = false;
+  } else if (n % 2 === 0) {
+    NT = false;
+  } else {
+    // Vì 2 là số nguyên tố chẵn duy nhất nên tạo biến i bắt đầu từ 3, điều kiện là i <= căn bậc 2 của n, với bước nhảy là 2 để chạy vòng lặp
+    for (var i = 3; i <= Math.sqrt(n); i += 2) {
+      // Nếu n chia i có dư bằng 0 thì biến cờ hiệu trả về false và lập tức dừng vòng lặp
+      if (n % i === 0) {
+        NT = false;
+        break;
+      }
+    }
+  }
+  return NT;
 }
